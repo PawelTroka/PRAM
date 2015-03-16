6bf6edb00080a8ef894e7dec812f81b2c4f1d57a
PTroka <pawel.troka@outlook.com>
2015-03-16 17:23:40 +0100

    First

---
+++ PRAM Machine/Gui/SizeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace PRAM_Machine.Gui {
+    class SizeConverter : IValueConverter {
+
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            int result = int.Parse(value.ToString());
+            int offset = int.Parse(parameter.ToString());
+            return (result - offset).ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            int result = int.Parse(value.ToString());
+            int offset = int.Parse(parameter.ToString());
+            return (result + offset).ToString();
+        }
+
+        #endregion
+    }
+}
