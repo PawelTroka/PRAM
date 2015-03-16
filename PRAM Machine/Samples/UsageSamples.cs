d71657c40c8aca6e4ee282adfbcbf6641ebac305
PTroka <pawel.troka@outlook.com>
2015-03-16 17:48:12 +0100

    fsddasdassafsa

--- PRAM Machine/Samples/UsageSamples.cs
+++ PRAM Machine/Samples/UsageSamples.cs
@@ -8,11 +8,11 @@ namespace PRAM_Machine.Samples {
         static void Main() {
             //SimulatorGui.Run(FastAddition.FastAdditionSetup.Setup(10));
             //SimulatorGui.Run(VectorAddition.VectorAdditionSetup.Setup(15));
-            SimulatorGui.Run(FastAdditionDivideAndConquer.FastAdditionDivideAndConquerSetup.Setup(20));
+            //SimulatorGui.Run(FastAdditionDivideAndConquer.FastAdditionDivideAndConquerSetup.Setup(20));
             //SimulatorGui.Run(LogicalAnd.LogicalAndSetup.Setup(5));
             //SimulatorGui.Run(MatrixMultiplication.MatrixMultiplicationSetup.Setup(3));
             //SimulatorGui.Run(Sorting.SortingSetup.Setup(4));
-            //SimulatorGui.Run(ListRanking.ListRankingSetup.Setup(10));
+            SimulatorGui.Run(ListRanking.ListRankingSetup.Setup(10));
         }
     }
 }
